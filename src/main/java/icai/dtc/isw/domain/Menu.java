2b779b663cce59a293e59a866bcc0b6b38e39da9
carlo <202302444@alu.comillas.edu>
2025-11-02 13:10:53 +0100

    Atributos setters y getters clase Menu

--- src/main/java/icai/dtc/isw/domain/Menu.java
+++ src/main/java/icai/dtc/isw/domain/Menu.java
@@ -1,10 +1,43 @@
 package icai.dtc.isw.domain;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Menu {
 
+    private final int size = 2;
+    private Map<String, Receta> recetas = new HashMap<>(size);;
     // menu se compone de dos recetas
     // se usará un algorítmo de knapsack pará encontrar la combinacion de dos recetas que resulta en el mejor menu
     // menu tendra el metodo crear un menu distinto
     // menu tendra el metodo devolver informacion del menu
+
+    public Menu(Receta receta, Receta receta2) {
+        recetas.put("comida",receta);
+        recetas.put("cena",receta2);
+    }
+
+    public Map<String,Receta> getRecetas() {
+        return recetas;
+    }
+
+    public Receta getComida() {
+        return recetas.get("comida");
+    }
+
+    public Receta getCena() {
+        return recetas.get("cena");
+    }
+
+    public void setComida(Map<String, Receta> recetas,  Receta receta) {
+        recetas.put("comida",receta);
+    }
+
+    public void setCena(Receta receta) {
+        recetas.put("cena",receta);
+    }
+
+
 
 }
