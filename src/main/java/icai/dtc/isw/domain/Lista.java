156316fd480f22d5df31ccebbbcaac5453ea60b4
Juan Torres <juanitotorrescolomina@gmail.com>
2025-11-06 19:18:20 +0100

    correcciones y controladores

--- src/main/java/icai/dtc/isw/domain/Lista.java
+++ src/main/java/icai/dtc/isw/domain/Lista.java
@@ -12,24 +12,24 @@ public class Lista {
 
     public Lista(int semana) {
         this.semana = semana;
-        rellenarProductos();
+        // rellenarProductos();
     }
 
     // rellena la lista de los productos para el menu de esa semana
-    public void rellenarProductos() {
-        productos = new ArrayList<>();
-        MenuSemanal menu = new MenuSemanal();
-        ArrayList<Receta> recetas = menu.getRecetasMenu();
-        for(Receta receta: recetas){
-            Map<String, Ingrediente> ingredienteMap = receta.getIngredientes();
-            for(String nombre : ingredienteMap.keySet()){
-                Producto producto = ProductoDAO.getProductoName(nombre);
-                if(productos.contains(producto)){
-                    productos.get(productos.indexOf(producto)).repiteProd();
-                }   else{productos.add(producto);}
-            }
-        }
-    }
+//    public void rellenarProductos() {
+//        productos = new ArrayList<>();
+//        MenuSemanal menu = new MenuSemanal();
+//        ArrayList<Receta> recetas = menu.getRecetasMenu();
+//        for(Receta receta: recetas){
+//            Map<String, Ingrediente> ingredienteMap = receta.getIngredientes();
+//            for(String nombre : ingredienteMap.keySet()){
+//                Producto producto = ProductoDAO.getProductoName(nombre);
+//                if(productos.contains(producto)){
+//                    productos.get(productos.indexOf(producto)).repiteProd();
+//                }   else{productos.add(producto);}
+//            }
+//        }
+//    }
 
     public ArrayList<Producto> getProductos() {
         return this.productos;
